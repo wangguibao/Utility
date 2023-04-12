c2734fd4950355b2a1deb15faea681a3f3077d03
Wang Guibao <wang_guibao@163.com>
2023-04-12 14:42:32 +0800

    Create threadpool.h

---
+++ threadpool/threadpool.h
@@ -0,0 +1,37 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+#include <functional>
+#include <queue>
+#include <mutex>
+
+class Task {
+public:
+    std::function<void(void* arg)> run;
+    void* arg;
+};
+
+class ThreadPool
+{
+public:
+    ThreadPool(int thread_num);
+
+    ~ThreadPool();
+
+public:
+    void addTask(Task task);
+
+private:
+    static void thread_func(ThreadPool *pool);
+
+private:
+    std::queue<Task> task_queue;
+    std::mutex mutex;
+    std::condition_variable cond;
+
+    std::vector<std::thread> thread_vec;
+    int thread_num;
+
+    bool running = false;
+};
+
+#endif // THREADPOOL_H
